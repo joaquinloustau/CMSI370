3ba752d38c40b5ecaaa31b07a4662011ebcee962
jloustau <jloustau@lion.lmu.edu>
2014-12-16 19:40:50 -0800

    Cleaning up folder

--- boxes-touch/drag-and-drop-demonstration.js
+++ boxes-touch/drag-and-drop-demonstration.js
@@ -1,91 +1,47 @@
 (function($) {
-  var itemsdropped = 0;
-  var itemMoved = function () {
-    $('#basket').text("Items Dropped (" + itemsdropped + ")");
-  }
+  $.fn.drags = function() {
+    var currentBox = null;
 
-  window.BoxesTouch = {
-    setDrawingArea: function (jQueryElements) {
-      jQueryElements
-      .addClass("origin-box")
-   
-      .each(function (index, element) {
-        element.addEventListener("touchmove", BoxesTouch.trackDrag, false);
-        element.addEventListener("touchend", BoxesTouch.endDrag, false);
-      })
-
-      .find("div.box").each(function (index, element) {
-        element.addEventListener("touchstart", BoxesTouch.startMove, false);
-        element.addEventListener("touchend", BoxesTouch.unhighlight, false);
-      });
-    },
-    setupDragState: function () {
-      $(".drawing-area .box")
-        .unbind("mousemove")
-        .unbind("mouseleave");
-    },
-
-    trackDrag: function (event) {
-      $.each(event.changedTouches, function (touch) {
-        event.preventDefault();
-        if (touch.target.movingBox) {
-          touch.target.movingBox.bottom = event.pageY - event.target.deltaY + touch.target.movingBox.height();
-          touch.target.movingBox.right = event.pageX - event.target.deltaX + touch.target.movingBox.width();
-          touch.target.movingBox.offset({
-            left: touch.pageX - touch.target.deltaX,
-            top: touch.pageY - touch.target.deltaY
-          });
-          
-          var inRange2 = 
-              touch.pageX > $('#destination-box').position().left &&
-              touch.pageX < $('#destination-box').position().left + $('#destination-box').width() &&
-              touch.pageY > $('#destination-box').position().top &&
-              touch.pageY < $('#destination-box').position().top + $('#destination-box').height(); 
-           
-          //Box border turns red when out of range, warns user that box will be deleted
-          if (inRange2) {
-            console.log('eliminar');
-            (touch.target.movingBox).addClass("delete-box deletebox-highlight");
-          } else {
-            (touch.target.movingBox).removeClass("delete-box deletebox-highlight");
-          }
-        }
-      });
-      event.preventDefault();
-    },
-
-    endDrag: function (event) {
-      $.each(event.changedTouches, function (index, touch) {
-        if (touch.target.movingBox) {
-          touch.target.movingBox.unhighlight;
-          touch.target.movingBox = null;
-        }
-      });
-    },
-
-    unhighlight: function () {
-      $(this).removeClass("box-highlight");
-      if ($(this).hasClass("delete-box")) {
-        $(this).remove();
-        itemsdropped++;
-        itemMoved();
-      };
-    },
-
-    startMove: function (event) {
-      $.each(event.changedTouches, function (index, touch) {
-      $(touch.target).addClass("box-highlight");
+    $('.box').on("mousedown", function(e) {
+      currentBox = $(this);
+      currentBox.addClass('draggable');
       
-      var jThis = $(touch.target),
-          startOffset = jThis.offset();
-          touch.target.movingBox = jThis;
-          touch.target.deltaX = touch.pageX - startOffset.left;
-          touch.target.deltaY = touch.pageY - startOffset.top;
+      var drg_h = currentBox.outerHeight(),
+          drg_w = currentBox.outerWidth(),
+          pos_y = currentBox.offset().top + drg_h - e.pageY,
+          pos_x = currentBox.offset().left + drg_w - e.pageX;
+      
+      $(document).on("mousemove", function(e) {
+        currentBox.offset({
+          top: e.pageY + pos_y - drg_h,
+          left: e.pageX + pos_x - drg_w
+        });
+
+        var inRange = 
+          e.pageX > $('.destination-box').offset().left &&
+          e.pageX < ($('.destination-box').offset().left + $('.destination-box').width()) &&
+          e.pageY > $('.destination-box').offset().top &&
+          e.pageY < ($('.destination-box').offset().top + $('.destination-box').height());
+
+        if (inRange) {
+          currentBox.addClass("delete-box deletebox-highlight");
+        } else {
+          currentBox.removeClass("delete-box deletebox-highlight");
+        } 
+      
+      }).on("mouseup", function() {
+        $(this).off("mousemove"); // Unbind events from document
+        if (currentBox.hasClass("delete-box")) {
+          currentBox.remove();
+          inDestinationBox++;
+        };
       });
-      event.stopPropagation();
-    }
+      e.preventDefault(); // disable selection
+      
+    }).on("mouseup", function() {
+      currentBox.removeClass('draggable');
+    });
   };
-  $.fn.boxes = function () {
-    BoxesTouch.setDrawingArea(this);
-  };
-}(jQuery));
+})(jQuery);
+
+
--- boxes-touch/drag-and-drop.js
+++ boxes-touch/drag-and-drop.js
@@ -1,86 +1,47 @@
-(function($)
-{
-	
-	// Holds the current item being dragged
-	var currentDrag = "";
-	var ruleChecked = false;
-	var dragCounter = 0;
-	
-	// Current mouse position (at all times!)
-	var mouseX = 0;
-	var mouseY = 0;
-	
-	// May be used to adjust offset of the ghost item
-	var liftX = 0;
-	var liftY = 0;
-	
-	$(document).bind('mousemove touchmove', function(e){
-		mouseX = e.pageX;
-		mouseY = e.pageY;
-		$("#ghost").css({ top: (mouseY-liftY)+"px", left: (mouseX-liftX)+"px" });
-	}); 
+(function($) {
+  $.fn.drags = function() {
+    var currentBox = null;
 
-	function IsValidDrop(id) {
-		var returnValue = false;
-		var currentDragParent = "#" + $(currentDrag).parent().attr("id");
-		if (currentDragParent == id) {
-			if (ruleChecked) {
-				returnValue = true;
-			} else {
-				returnValue = false;
-			}
-		} else {
-			returnValue = true;
-		}
-		// Prevents the rule from failing on the same item twice in a row
-		ruleChecked = true;
-		
-		return returnValue;
-	}
+    $('.box').on("mousedown", function(e) {
+      currentBox = $(this);
+      currentBox.addClass('draggable');
+      
+      var drg_h = currentBox.outerHeight(),
+          drg_w = currentBox.outerWidth(),
+          pos_y = currentBox.offset().top + drg_h - e.pageY,
+          pos_x = currentBox.offset().left + drg_w - e.pageX;
+      
+      $(document).on("mousemove", function(e) {
+        currentBox.offset({
+          top: e.pageY + pos_y - drg_h,
+          left: e.pageX + pos_x - drg_w
+        });
 
-	function ProcessDragEvent(id, dragSelector, dropSelector, statusSelector, selectedClass, activeClass) {
-		
-		// Set the currentDrag item
-		currentDrag = id;
-		ruleChecked = false;
-		
-		// Remove the selected class from all draggable items 
-		// and add it to the current item
-		$(dragSelector).removeClass(selectedClass);
-		$(currentDrag).addClass(selectedClass);
-		
-		$("#ghost").remove();
-		$(currentDrag).clone().attr("id", "ghost").css({ position: "absolute" }).appendTo("body").fadeTo(0, 0.5);
-		
-		// Outputs the current draggable item onto the page
-		if (statusSelector.length > 0) {
-			$(statusSelector).find("#dragging").html(currentDrag);
-		}
-		
-		// If the current draggable item isn't blank, highlight 
-		// the droppable zones
-		if (currentDrag.length > 1) {
-			$(dropSelector).addClass(activeClass);
-		}
-	}
-	
-	function ProcessDropEvent(id, dragSelector, dropSelector, statusSelector, selectedClass, activeClass) {
-	
-		if (IsValidDrop(id) && $(id).hasClass(activeClass)) {
-			// If the current draggable item isn't blank, we will
-			// move the element and place it inside the target 
-			// droppable element
-			if (currentDrag.length > 1) {
-				$("#ghost").remove();
-				$(currentDrag).removeClass(selectedClass);
-				$(currentDrag).remove().prependTo(id);
-				ProcessDragEvent("", dragSelector, dropSelector, statusSelector, selectedClass, activeClass);
-				if (statusSelector.length > 0) {
-					$(statusSelector).find("#dropping").html(id);
-				}
-			}
-			
-			// Remove the highlighting of droppable zones
-			$(dropSelector).removeClass(activeClass);
-		}
-	}
+        var inRange = 
+          e.pageX > $('.destination-box').offset().left &&
+          e.pageX < ($('.destination-box').offset().left + $('.destination-box').width()) &&
+          e.pageY > $('.destination-box').offset().top &&
+          e.pageY < ($('.destination-box').offset().top + $('.destination-box').height());
+
+        if (inRange) {
+          currentBox.addClass("delete-box deletebox-highlight");
+        } else {
+          currentBox.removeClass("delete-box deletebox-highlight");
+        } 
+      
+      }).on("mouseup", function() {
+        $(this).off("mousemove"); // Unbind events from document
+        if (currentBox.hasClass("delete-box")) {
+          currentBox.remove();
+          inDestinationBox++;
+        };
+      });
+      e.preventDefault(); // disable selection
+      
+    }).on("mouseup", function() {
+      currentBox.removeClass('draggable');
+    });
+  };
+})(jQuery);
+
+
